68c4d631ef5a82cdceb39c4203c9aa934d8ac5b5
mitanshu5911 <mitanshubansal1234@gmail.com>
2026-03-19 19:36:18 +0530

    id verfefication error fix

--- src/utils/idVerification.js
+++ src/utils/idVerification.js
@@ -9,28 +9,31 @@ export const verifyIdCard = ({
   const text = normalizeText(ocrText);
   console.log("Normalized OCR Text:", text);
   const checks = {
-    university:
-      text.includes("CHITKARA") ||
-      text.includes("UNIVERSITY"),
+  university:
+    fuzzyMatch(text, "CHITKARA") ||
+    fuzzyMatch(text, "UNIVERSITY"),
 
-    name: fuzzyMatch(text, userName.toUpperCase()),
+  name: fuzzyMatch(text, userName.toUpperCase()),
 
-    roll: text.includes(rollNumber),
-
-    structure:
-      (
-        text.includes("SESSION") ||
-        text.includes("BLOOD")
-      ) &&
-      (
-        text.includes("ID") || text.includes("CARD") || text.includes("IDENTITY")||text.includes("FATHER")||text.includes("MOTHER")
-      ),
-  };
-
+  roll: text.includes(rollNumber),
+  structure:
+    (
+      fuzzyMatch(text, "SESSION") ||
+      fuzzyMatch(text, "BLOOD")
+    ) &&
+    (
+      fuzzyMatch(text, "ID") ||
+      fuzzyMatch(text, "CARD") ||
+      fuzzyMatch(text, "IDENTITY") ||
+      fuzzyMatch(text, "FATHER") ||
+      fuzzyMatch(text, "MOTHER")
+    ),
+};
   const allPassed = Object.values(checks).every(Boolean);
-
+  console.log(checks);
   return {
     verified: allPassed,
     checks,
+    
   };
 };
